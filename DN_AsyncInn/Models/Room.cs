227f19a41b8fd2a3f6005edec522077ca15b2d45
Jason Hiskey <jasonhi@crazyredhead.com>
2019-01-27 13:59:17 -0800

    Completed functionality now going to stylize it...

--- DN_AsyncInn/Models/Room.cs
+++ DN_AsyncInn/Models/Room.cs
@@ -1,22 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DN_AsyncInn.Models
 {
-    public enum Layout { Studio, OneBedroom, TwoBedroom }
+    
 
     public class Room
     {
         
         public int ID { get; set; }
+
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Layout Must Be Selected")]
+        [EnumDataType(typeof(Layout))]
         public Layout Layout { get; set; }
 
         //Navigation Properties
 
         public ICollection<HotelRoom> HotelRooms { get; set; }
         public ICollection<RoomAmenities> RoomAmenities { get; set; }
+    }
+
+    public enum Layout
+    {
+        [Display(Name = "Studio")]
+        Studio = 0,
+        [Display(Name = "One Bedroom")]
+        OneBedroom,
+        [Display(Name = "Two Bedroom")]
+        TwoBedroom
     }
 }
