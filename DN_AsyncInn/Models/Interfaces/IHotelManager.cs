0d12d9e74a376d563417d189b9fa4d376327ba6d
Jason Hiskey <jasonhi@crazyredhead.com>
2019-01-29 09:23:21 -0800

    Completed Hotel dependency injection... Painfully

---
+++ DN_AsyncInn/Models/Interfaces/IHotelManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DN_AsyncInn.Models.Interfaces
+{
+     public interface IHotelManager
+    {
+        //Create Hotel
+        Task CreateHotel(Hotel hotel);
+
+        //Read Hotel
+        Task<Hotel> GetHotel(int id);
+
+        Task<IEnumerable<Hotel>> GetHotels();
+
+        Task<Hotel> GetHotel(string Name);
+
+        //Update Hotel
+        void UpdateHotel(Hotel hotel);
+
+        //DeleteHotel
+        void DeleteHotel(Hotel hotel);
+
+        void DeleteHotel(int id);
+    }
+}
---
+++ DN_AsyncInn/Models/Services/HotelManagmentService.cs
@@ -0,0 +1,62 @@
+﻿using DN_AsyncInn.Data;
+using DN_AsyncInn.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DN_AsyncInn.Models.Services
+{
+    public class HotelManagmentService : IHotelManager
+    {
+        private AsyncInnDbContext _context { get; }
+
+        // Connects to DB
+        public HotelManagmentService(AsyncInnDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateHotel(Hotel hotel)
+        {
+            _context.Hotels.Add(hotel);
+            await _context.SaveChangesAsync();
+        }
+            
+        public void DeleteHotel(Hotel hotel)
+        {
+            _context.Hotels.Remove(hotel);
+            _context.SaveChanges();
+        }
+
+        public void DeleteHotel(int id)
+        {
+            Hotel hotel = _context.Hotels.FirstOrDefault(h => h.ID == id);
+            _context.Hotels.Remove(hotel);
+            _context.SaveChanges();
+        }
+
+        public async Task<Hotel> GetHotel(int id)
+        {
+            
+            return await _context.Hotels.FirstOrDefaultAsync(h => h.ID == id);
+        }
+
+        public async Task<Hotel> GetHotel(string name)
+        {
+            return await _context.Hotels.FirstOrDefaultAsync(h => h.Name == name);
+        }
+
+        public void UpdateHotel(Hotel hotel)
+        {
+            _context.Hotels.Update(hotel);
+            _context.SaveChanges();
+        }
+
+        public async Task<IEnumerable<Hotel>> GetHotels()
+        {
+            return await _context.Hotels.ToListAsync();
+        }
+    }
+}
